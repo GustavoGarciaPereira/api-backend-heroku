bd771b516cc36e608b4ff5ca2982bd5c5bbf49f2
GustavoGarciaPereira <gusgurtavo@gmail.com>
2020-04-29 22:31:24 -0300

    configuração

---
+++ myapi/myapp/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('musics/', views.MusicList.as_view(), name='music-list'),
+]
---
+++ myapi/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework import generics
+from .models import Music
+from .serializers import MusicSerializer
+
+# Create your views here.
+class MusicList(generics.ListCreateAPIView):
+
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
