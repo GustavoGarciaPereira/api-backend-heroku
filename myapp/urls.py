1880415f76a43382860dc93919f4694ec3c9d28f
gustavoParciaPereira <you@example.com>
2020-07-19 13:14:10 -0300

    filtros na api

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,20 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r"programadores/?", views.ProgramadorView)
+
+
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('musics/', views.MusicList.as_view(), name='music-list'),
-    path('programador/', views.ProgramadorList.as_view(), name='programador-list'),
+    #path('programador/', views.ProgramadorList.as_view(), name='programador-list'),
+    path('programador/', include(router.urls), name='programador-list'),
+    
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,10 @@ from rest_framework import generics
 from .models import Music, Programador
 from .serializers import MusicSerializer,ProgramadorSerializer
 from django.views.generic import TemplateView
+from rest_framework.views import APIView
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+
 # Create your views here.
 class MusicList(generics.ListCreateAPIView):
 
@@ -13,12 +17,40 @@ class MusicList(generics.ListCreateAPIView):
     serializer_class = MusicSerializer
 
 
-class ProgramadorList(generics.ListCreateAPIView):
+#class ProgramadorList(generics.ListCreateAPIView):
+#
+#    queryset = Programador.objects.all()
+#    serializer_class = ProgramadorSerializer
+#class ProgramadorView(APIView):
+#    serializer_class = ProgramadorSerializer
+#    def get(self,request):
+#        dados = {}
+#        programadores = Programador.objects.all()
+#        print("<>",programadores)
+#        dados['dados'] = programadores
+#        return Response(dados,status=status.HTTP_200_OK)
+
+
+
+
+class ProgramadorView(viewsets.ModelViewSet):
 
     queryset = Programador.objects.all()
     serializer_class = ProgramadorSerializer
 
-
+    def get_queryset(request):
+        nome = request.request.query_params.get("nome")
+        liguagem_atualmente = request.request.query_params.get("liguagem_atualmente")
+        
+        if nome:
+            programador = Programador.objects.filter(nome__icontains=nome)
+            return programador
+        elif liguagem_atualmente:
+            programador = Programador.objects.filter(liguagem_atualmente__icontains=liguagem_atualmente)
+            return programador
+            
+        programador = Programador.objects.filter()
+        return programador 
 
 
 class Home(TemplateView):
